5273ea38e7d42cf594c8edaa0adb877bcb3ad8fe
Paulo Corazza <corazza.paulovinicius@gmail.com>
2020-05-11 15:38:41 -0300

    top

--- js/index.js
+++ js/index.js
@@ -76,3 +76,26 @@ methods: {
 
 })
 $("#contactForm").validate();
+
+  var button = document.getElementById("top");
+
+  window.onscroll = function(){
+    scrollFunction()
+  };
+
+  function scrollFunction(){
+    if(document.body.scrollTop < 20 || document.documentElement.scrollTop > 20){
+      button.style.display = "block";
+    }
+    else{
+      button.style.display = "none";
+    }
+  }
+
+  function toTheTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
